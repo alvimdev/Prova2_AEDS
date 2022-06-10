849a167f8fff71e36bd183b93030f1d000898048
Bernardo Souza <83983141+alvimdev@users.noreply.github.com>
2022-06-10 10:24:53 -0300

    Update main.c

--- Prova2/main.c
+++ Prova2/main.c
@@ -6,25 +6,79 @@
 #include <string.h>
 #include <stdlib.h>
 
-void BubbleSort(int vetor[], int tamanho){
-	int aux, i, j;
-	for(j=tamanho-1; j>=1; j--){
-		for(i=0; i<j; i++){
-			if(vetor[i]>vetor[i+1]){
-				aux=vetor[i];
-                vetor[i]=vetor[i+1];
-                vetor[i+1]=aux;
-            }
-        }
+#define STACK (10)
+#define QUEUE (20)
+#define P_QUEUE (30)
+
+void bubbleSort(int ∗v, int tam){
+	int c, cc, aux;
+	for(i = t; i > 0; i--){
+		for(j = 0; j < i; j++){
+		}
+	}
+}
+
+int queue_(int* e_in, int* e_out, int qtd_e, int qtd_s){
+    for (int i = 0; i < qtd_s; i++)
+    {
+        if(e_in[i] != e_out[i];){
+		return 0;
+	}
     }
+    return 1;
+}
+
+int stack_(int* e_in, int* e_out, int qtd_e, int qtd_s){
+	int aux;
+	int buffer[qtd_s];
+	
+	for(int i = qtd_s; i > 0; i--){
+		for(int k = 0; k < qtd_s; k++){
+			buffer[k] = e_out[i];
+		}
+	}
+	for (int c = 0; c < qtd_s; c++)
+    {
+        if(e_in[c] != buffer[c];){
+		return 0;
+	}
+    }
+    return 1;
+}
+
+int queue_p(int* e_in, int* e_out, int qtd_e, int qtd_s){
+	
 }
 
 void compara_e(int* e_in, int* e_out, int qtd_e, int qtd_s){
-    for (int i = 0; i < qtd_s; i++)
-    {
-        
-    }
-    
+	int value = 0, r=0;
+	
+	if(stack_(int* e_in, int* e_out, int qtd_e, int qtd_s)){
+		value = STACK;
+		r++;
+	}
+	
+	if(queue_(int* e_in, int* e_out, int qtd_e, int qtd_s)){
+		value = QUEUE;
+		r++;
+	}
+	
+	if(queue_p(int* e_in, int* e_out, int qtd_e, int qtd_s)){
+		value = P_QUEUE;
+		r++;
+	}
+	if((r == 3)||(r == 2)){
+		(void)printf("not sure");
+	} else if(value == STACK){
+		(void)printf("stack");
+	} else if(value == QUEUE){
+		(void)printf("queue");
+	} else if(value == PQUEUE){
+		(void)printf("priority queue");
+	} else{
+		(void) printf("impossible");
+	}
+	
 }
 
 int main(int agrc, char** agrv){
@@ -54,6 +108,7 @@ int main(int agrc, char** agrv){
                 fscanf(arq, "%d %d", &op, &val);
                 if(op == 1){
                     e_entrada[i] = val;
+		    qtd_e++;
                 }
                 if(op == 2){
                     e_saida[i] = val;
