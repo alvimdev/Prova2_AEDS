fcc16ac300ca1abccd39d44320d5e9a9b66d64a4
Bernardo Souza <83983141+alvimdev@users.noreply.github.com>
2022-06-10 12:50:24 -0300

    Update main.c

--- Prova2/main.c
+++ Prova2/main.c
@@ -10,36 +10,51 @@
 #define QUEUE (20)
 #define P_QUEUE (30)
 
-void bubbleSort(int ∗v, int tam){
-	int c, cc, aux;
-	for(i = t; i > 0; i--){
-		for(j = 0; j < i; j++){
+void bubbleSort(int v[], int tam){
+	int c, cc, aux=0;
+	for(c = tam-1; c > 0; c--){
+		for(cc = 0; cc < c; cc++){
+            if (v[c] > v[cc]) {
+                aux = v[c];
+                v[c] = v[cc];
+                v[cc] = aux;
+            }
 		}
 	}
+}
+
+int checa_elem(int * e_in, int qtd_e, int N){
+    for (int k = 0; k < qtd_e; k++)
+    {
+        if (e_in[k] == N)
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int queue_(int* e_in, int* e_out, int qtd_e, int qtd_s){
     for (int i = 0; i < qtd_s; i++)
     {
-        if(e_in[i] != e_out[i];){
+        if(e_in[i] != e_out[i]){
 		return 0;
-	}
+	    }
     }
     return 1;
 }
 
 int stack_(int* e_in, int* e_out, int qtd_e, int qtd_s){
-	int aux;
+	int aux, j = qtd_s-1;
 	int buffer[qtd_s];
 	
-	for(int i = qtd_s; i > 0; i--){
-		for(int k = 0; k < qtd_s; k++){
-			buffer[k] = e_out[i];
-		}
+	for(int i = 0; i < qtd_s; i++){
+		buffer[i] = e_out[j];
+        j--;
 	}
 	for (int c = 0; c < qtd_s; c++)
     {
-        if(e_in[c] != buffer[c];){
+        if(e_in[c] != buffer[c]){
 		return 0;
 	}
     }
@@ -47,36 +62,64 @@ int stack_(int* e_in, int* e_out, int qtd_e, int qtd_s){
 }
 
 int queue_p(int* e_in, int* e_out, int qtd_e, int qtd_s){
-	
+    int buffer[qtd_s];
+
+	for (int i = 0; i < qtd_s; i++)
+    {
+        buffer[i] = e_out[i];
+    }
+    bubbleSort(buffer, qtd_s);
+    for (int c = 0; c < qtd_s; c++)
+    {
+        if(e_out[c] != buffer[c]){
+		return 0;
+	}
+    }
+    return 1;
 }
 
 void compara_e(int* e_in, int* e_out, int qtd_e, int qtd_s){
-	int value = 0, r=0;
+	int value = 0, r = 0;
+
+    if (qtd_e < qtd_s)
+    {
+        (void)printf("impossible\n");
+        return;
+    }
+    
+
+    for (int i = 0; i < qtd_s; i++)
+    {
+        if(checa_elem(e_in, qtd_e, e_out[i]) == 0){
+            (void)printf("impossible\n");
+            return;
+        }   
+    }
+    
 	
-	if(stack_(int* e_in, int* e_out, int qtd_e, int qtd_s)){
+	if((stack_(e_in, e_out, qtd_e, qtd_s)) == 1){
 		value = STACK;
 		r++;
 	}
-	
-	if(queue_(int* e_in, int* e_out, int qtd_e, int qtd_s)){
+	if((queue_(e_in, e_out, qtd_e, qtd_s)) == 1){
 		value = QUEUE;
 		r++;
 	}
 	
-	if(queue_p(int* e_in, int* e_out, int qtd_e, int qtd_s)){
+	if((queue_p(e_in, e_out, qtd_e, qtd_s)) == 1){
 		value = P_QUEUE;
 		r++;
 	}
-	if((r == 3)||(r == 2)){
-		(void)printf("not sure");
-	} else if(value == STACK){
-		(void)printf("stack");
-	} else if(value == QUEUE){
-		(void)printf("queue");
-	} else if(value == PQUEUE){
-		(void)printf("priority queue");
+	if((r > 1)){
+		(void)printf("not sure\n");
+	} else if((value == STACK) && (r == 1)){
+		(void)printf("stack\n");
+	} else if((value == QUEUE) && (r == 1)){
+		(void)printf("queue\n");
+	} else if((value == P_QUEUE) && (r == 1)){
+		(void)printf("priority queue\n");
 	} else{
-		(void) printf("impossible");
+		(void) printf("impossible\n");
 	}
 	
 }
@@ -85,7 +128,7 @@ int main(int agrc, char** agrv){
     //Variaveis
     int * e_entrada;
     int * e_saida;
-    int qtd_op, qtd_e, qtd_s, op, val;
+    int qtd_op = 0, qtd_e = 0, qtd_s = 0, op, val;
     //Arquivo(s)
     FILE * arq;
     //Caminho(s)
@@ -93,7 +136,7 @@ int main(int agrc, char** agrv){
 
     arq = fopen(path, "r");
     if(arq != NULL){
-        while (!EOF)
+        while (!feof(arq))
         {
             fscanf(arq, "%d",  &qtd_op);
 
@@ -107,15 +150,16 @@ int main(int agrc, char** agrv){
             {
                 fscanf(arq, "%d %d", &op, &val);
                 if(op == 1){
-                    e_entrada[i] = val;
-		    qtd_e++;
+                    e_entrada[qtd_e] = val;
+		            qtd_e++;
                 }
                 if(op == 2){
-                    e_saida[i] = val;
+                    e_saida[qtd_s] = val;
                     qtd_s++;
                 }
-                compara_e(e_entrada, e_saida, qtd_e, qtd_s);
             }
+            
+            compara_e(e_entrada, e_saida, qtd_e, qtd_s);
 
             free(e_entrada);
             free(e_saida);
