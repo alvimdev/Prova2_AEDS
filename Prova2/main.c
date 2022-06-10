dfe3ee2b21ee140075165aedcc29aa6d41db455c
Bernardo Souza <83983141+alvimdev@users.noreply.github.com>
2022-06-09 21:05:36 -0300

    Update main.c

--- Prova2/main.c
+++ Prova2/main.c
@@ -12,55 +12,26 @@ void BubbleSort(int vetor[], int tamanho){
 		for(i=0; i<j; i++){
 			if(vetor[i]>vetor[i+1]){
 				aux=vetor[i];
-                    vetor[i]=vetor[i+1];
-                    vetor[i+1]=aux;
+                vetor[i]=vetor[i+1];
+                vetor[i+1]=aux;
             }
         }
     }
 }
 
-void compara_e(int * e_in, int * e_out, int qtd_op){
-    int e_out_rev[qtd_op];
-    int e_out_buff[qtd_oṕ];
-
-    if (memcmp(&e_in, &e_out, sizeof(e_in)) == 0)
+void compara_e(int* e_in, int* e_out, int qtd_e, int qtd_s){
+    for (int i = 0; i < qtd_s; i++)
     {
-        (void)printf("queue");
-    }
-
-    for (int i = 0; i < qtd_op; i++)
-    {
-        for (int j = qtd_op; j > 0; j--)
-        {
-            e_out_rev[i] = e_out[j];
-        }
-    }
-    if (memcmp(&e_in, &e_out_rev, sizeof(e_in)) == 0)
-    {
-        (void)printf("stack");
+        
     }
     
-    for (int c = 0; c < qtd_op; c++)
-    {
-        for (int v = qtd_op; v > 0; v++)
-        {
-            e_out_buff[i] = e_out[j];
-        }
-    if (memcmp(&BubbleSort(e_out_buff), &e_out, sizeof(e_out)) == 0))
-    {
-        if(memcmp(&e_in, &e_out, sizeof(e_in)) == 0)){
-            (void)printf("priority queue");
-        }
-    }
-
-    (void)printf("impossible");
 }
 
 int main(int agrc, char** agrv){
     //Variaveis
     int * e_entrada;
     int * e_saida;
-    int qtd_op, op, ct, n_lines=1;
+    int qtd_op, qtd_e, qtd_s, op, val;
     //Arquivo(s)
     FILE * arq;
     //Caminho(s)
@@ -75,15 +46,24 @@ int main(int agrc, char** agrv){
             e_entrada = (int*) malloc(sizeof(int) * qtd_op);
             e_saida = (int*) malloc(sizeof(int) * qtd_op);
 
+            qtd_s = 0;
+            qtd_e = 0;
+
             for (int i = 0; i < qtd_op; i++)
             {
-                
+                fscanf(arq, "%d %d", &op, &val);
+                if(op == 1){
+                    e_entrada[i] = val;
+                }
+                if(op == 2){
+                    e_saida[i] = val;
+                    qtd_s++;
+                }
+                compara_e(e_entrada, e_saida, qtd_e, qtd_s);
             }
-            compara_e(e_entrada, e_saida, qtd_op);
 
             free(e_entrada);
             free(e_saida);
-            n_lines += qtd_op;
         }
     }
 
